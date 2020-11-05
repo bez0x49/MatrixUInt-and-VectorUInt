2e5f351d662eb2647a35f2d39d95f860eebbd685
bez0x49 <nevedrb@gmail.com>
2020-11-05 22:05:20 +0200

    Precompiled header

--- include/stdafx.h
+++ include/stdafx.h
@@ -1,5 +1,16 @@
+/**
+ * @file stdafx.h
+ * @author bez0x49 (bez0x49@github.com, nevedrb@gmail.com)
+ * @brief Precompiled header
+ * @version 0.1
+ * @date 2020-11-05
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
 #pragma once
 
-#include <algorithm>
-#include <exception>
-#include <iostream>
+#include <algorithm>  // std::cout
+#include <exception>  // std::exception
+#include <iomanip>    // std::boolalpha
+#include <iostream>   // std::cout, std::cin, std::endl
